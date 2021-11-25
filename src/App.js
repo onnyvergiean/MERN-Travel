7d9810299f647cc68d0386d33dff1e9ec80eb316
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2021-11-25 21:26:59 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./assets/scss/style.scss";
+
+function App() {
+  return <div>Hello World</div>;
+}
+
+export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+import elements from "elements/Button";
+export default function LandingPage() {
+  return <div></div>;
+}
