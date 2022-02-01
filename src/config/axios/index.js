bd532e9883bcd74afc6490f2da205c7751cb93fe
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-02-01 23:19:58 +0700

    Fix Import ErrorResponseHandler

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import errorResponseHandler from "./errorResponseHandler";
 const instance = axios.create({
   baseURL: `${process.env.REACT_APP_HOST}/api/v1/member`,
 });
